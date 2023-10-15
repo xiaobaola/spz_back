6bb566dbda11af24b75bcf34c1559ec8acfe2a54
xiaobao_lala <1651041574@qq.com>
2023-10-15 17:46:30 +0800

    废品分类后端完善

--- src/main/java/com/spz/controller/ScrapTypeController.java
+++ src/main/java/com/spz/controller/ScrapTypeController.java
@@ -9,9 +9,7 @@ import com.spz.entity.user.UserMessage;
 import com.spz.service.ScrapTypeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -22,23 +20,36 @@ public class ScrapTypeController {
     @Autowired
     private ScrapTypeService scrapTypeService;
 
+    /**
+     * @param
+     * @return @return {@link Res }<{@link ArrayList }<{@link ScrapType }>>
+     * @author last
+     * @describe
+     */
     @GetMapping("/list")
     public Res<ArrayList<ScrapType>> list2() {
         log.info("get 废品类型列表");
         ArrayList<ScrapType> scrapTypeArrayList = scrapTypeService.list2();
         return Res.success(scrapTypeArrayList);
     }
-    @GetMapping("/test")
-    public Res<ScrapType> test(){
-        scrapTypeService.insert2();
-        log.info("测试中");
-        ScrapType scrapType = new ScrapType();
-        return Res.success(scrapType);
+
+    @PutMapping
+    public Res<String> updateById(@RequestBody ScrapType scrapType) {
+        log.info("修改: {}", scrapType);
+        scrapTypeService.updateById(scrapType);
+        return Res.success("修改废品类型成功");
     }
-    @GetMapping("/login")
-    public Res<String> login(){
-        log.info("接收到登录请求");
-//        return Res.success("登录成功");
-        return Res.error("错误");
-    }
+//    @GetMapping("/test")
+//    public Res<ScrapType> test(){
+//        scrapTypeService.insert2();
+//        log.info("测试中");
+//        ScrapType scrapType = new ScrapType();
+//        return Res.success(scrapType);
+//    }
+//    @GetMapping("/login")
+//    public Res<String> login(){
+//        log.info("接收到登录请求");
+////        return Res.success("登录成功");
+//        return Res.error("错误");
+//    }
 }
