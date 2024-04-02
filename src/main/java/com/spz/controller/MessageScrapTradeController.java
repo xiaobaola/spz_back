81359623c7281f9d88dfe5d6ab6d0096bd1cd1c4
御魂123 <1059560373@qq.com>
2024-04-02 20:09:47 +0800

    更新

---
+++ src/main/java/com/spz/controller/MessageScrapTradeController.java
@@ -0,0 +1,28 @@
+package com.spz.controller;
+
+import com.spz.common.Res;
+import com.spz.entity.communicate.MessageScrapTrade;
+import com.spz.service.MessageScrapTradeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("spz/messageScrapTrade")
+public class MessageScrapTradeController {
+    @Autowired
+    private MessageScrapTradeService messageScrapTradeService;
+
+    @GetMapping("/{id}")
+    public Res<MessageScrapTrade> getMessageTradeId(@PathVariable Integer id){
+        return Res.success(messageScrapTradeService.getMessageTradeIdById(id));
+    }
+
+    @GetMapping("/{id}")
+    public Res<MessageScrapTrade> getScrapTradeId(@PathVariable Integer id){
+        return Res.success(messageScrapTradeService.getScrapTradeIdById(id));
+    }
+
+}
--- src/main/java/com/spz/service/MessageScrapTradeService.java
+++ src/main/java/com/spz/service/MessageScrapTradeService.java
@@ -3,6 +3,7 @@ package com.spz.service;
 import com.spz.entity.communicate.MessageScrapTrade;
 import org.apache.ibatis.annotations.Select;
 
+
 public interface MessageScrapTradeService {
 
     MessageScrapTrade getMessageTradeIdById(Integer id);
