8be79d3044c54aaad172aa5ae88d9c7e162c78f5
xiaobao_lala <1651041574@qq.com>
2024-04-09 21:49:59 +0800

    管理端新增信息条上传网页修改1

--- src/main/resources/backend/api/trade.js
+++ src/main/resources/backend/api/trade.js
@@ -36,7 +36,7 @@ const addMessageScrapTrade = (params) => {
 // 更改订单状态的接口
 const changeScrapTradeStatus = (params) => {
     return $axios({
-        url: `/spz/messageScrapTrade/ids`,
+        url: `/spz/scrapTrade/ids`,
         method: 'put',
         data: {...params}
     })
