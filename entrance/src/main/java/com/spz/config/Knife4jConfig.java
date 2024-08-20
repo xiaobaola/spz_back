910f05a3694ec36606bac418db79f48ef2404be5
xiaobao_lala <1651041574@qq.com>
2024-08-20 13:02:11 +0800

    docs(knife4j):优化Swagger3文档配置和分组
    重命名Knife4jConfig中的分组，并更新了OpenAPI的基本信息，以提升Swagger3文档的可用性和清晰度。修正了application-save.yml中与Swagger相关配置的格式。

--- entrance/src/main/java/com/spz/config/Knife4jConfig.java
+++ entrance/src/main/java/com/spz/config/Knife4jConfig.java
@@ -9,27 +9,20 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class Knife4jConfig {
     @Bean
-    public OpenAPI customOpenAPI() {
-
-        return new OpenAPI()
-                .info(new Info()
-                        .title("接口文档")
-                        .version("1.0")
-                        .description("接口文档")
-                );
-    }
-    /**
-     * 添加分组
-     * @return
-     */
-    @Bean
-    public GroupedOpenApi publicApi() {
+    public GroupedOpenApi adminApi() { // 创建了一个api接口的分组
         return GroupedOpenApi.builder()
-                .group("spz-messageTrade")
-                .pathsToMatch("/spz/messageTrade/**")
-                .packagesToScan("com.spz.messageTrade.controller")
+                .group("admin-api") // 分组名称
+                .pathsToMatch("/**") // 接口请求路径规则
                 .build();
     }
 
-
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info() // 基本信息配置
+                        .title("Knife4j整合Swagger3 Api接口文档") // 标题
+                        .description("Knife4j后端接口服务...") // 描述Api接口文档的基本信息
+                        .version("v1.0.0") // 版本
+                );
+    }
 }
